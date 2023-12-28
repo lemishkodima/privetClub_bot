9e086257a61ea91a2a2acd8da2db8e7869af217f
Lemishko Dmitry <81725793+lemishkodima@users.noreply.github.com>
2023-12-28 17:35:11 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,19 +11,6 @@ import logging
 from googleapiclient.discovery import build
 from google.oauth2.service_account import Credentials
 
-def append_data_to_sheet(user_data, spreadsheet_id, range_name):
-    """Добавляет данные пользователя в Google таблицу."""
-    creds = Credentials.from_service_account_file("maxim.json")
-    service = build('sheets', 'v4', credentials=creds)
-    sheet = service.spreadsheets()
-
-    request = sheet.values().append(spreadsheetId=spreadsheet_id, 
-                                    range=range_name, 
-                                    valueInputOption="USER_ENTERED", 
-                                    body={"values": [user_data]})
-    response = request.execute()
-    return response
-
 
 BOT_TOKEN = '6180592335:AAFLKZ60x7efxzPgmo70DIqkB7HrifkXgrs' 
 CHANNEL_ID =  -1001485074571
@@ -36,6 +23,20 @@ async def approve_request (chat_join: ChatJoinRequest, bot: Bot):
    user_data = [chat_join.from_user.id, chat_join.from_user.username, chat_join.from_user.first_name]
    append_data_to_sheet(user_data, "1nCSQBIwryKNs13N_9MH8C6OMMjHBCXiMhRs5Q6TkxtA", "A:C")
    await bot.send_message(chat_id=chat_join.from_user.id, text=msg, reply_markup=markup, disable_web_page_preview=True)
+
+
+def append_data_to_sheet(user_data, spreadsheet_id, range_name):
+    """Добавляет данные пользователя в Google таблицу."""
+    creds = Credentials.from_service_account_file("maxim.json")
+    service = build('sheets', 'v4', credentials=creds)
+    sheet = service.spreadsheets()
+
+    request = sheet.values().append(spreadsheetId=spreadsheet_id, 
+                                    range=range_name, 
+                                    valueInputOption="USER_ENTERED", 
+                                    body={"values": [user_data]})
+    response = request.execute()
+    return response
  
 
 async def start():
@@ -58,6 +59,3 @@ async def start():
 if __name__ == '__main__':
     with contextlib.suppress(KeyboardInterrupt, SystemExit):
         asyncio.run(start())
-
-  
-
